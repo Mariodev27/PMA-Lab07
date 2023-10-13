89b4e34dc70f338fd3a9ce2cce71fa351aee1a03
Mario Santisteban A <85888524+Mariodev27@users.noreply.github.com>
2023-10-13 18:01:47 -0500

    Add files via upload

---
+++ UsodeControlesSantisteban/UsodeControlesSantisteban/FirstViewController.swift
@@ -0,0 +1,77 @@
+//
+//  FirstViewController.swift
+//  UsodeControlesSantisteban
+//
+//  Created by Alumno on 13/10/23.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    
+    @IBOutlet weak var txtRpta: UITextField!
+    
+    @IBOutlet weak var txtNumero1: UITextField!
+    
+    @IBOutlet weak var txtNumero2: UITextField!
+    
+    var nro1:Double = 0
+    var nro2:Double = 0
+    var rpta:Double = 0
+    
+    //funcion de limpieza
+    func Limpiar() {
+        self.txtNumero1.text = "0"
+        self.txtNumero2.text = "0"
+        self.txtRpta.text = "0"
+    }
+    
+    //funcion de alerta
+    func mostrarAlerta(titulo: String, mensaje: String) {
+        let alerta = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
+        
+        let limpiar = UIAlertAction(title: "Limpiar", style: .default, handler: {(action) in
+                self.Limpiar()
+            })
+            
+        let cancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler:{(action) in
+                
+            })
+            alerta.addAction(limpiar)
+            alerta.addAction(cancelar)
+            present(alerta,animated: true, completion: nil)
+            }
+    
+    @IBAction func btnSumar(_ sender: Any) {
+        if Double(txtNumero1.text!) != nil && Double(txtNumero2.text!) != nil {
+            nro1 = Double(txtNumero1.text!)!
+            nro2 = Double(txtNumero2.text!)!
+            rpta = nro1 + nro2
+            print(rpta)
+                txtRpta.text = String(rpta)
+        }else{
+            mostrarAlerta(titulo: "Error", mensaje: "Debe introducir solo valores numericos")
+            print("Error al realizar la operacion")
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
